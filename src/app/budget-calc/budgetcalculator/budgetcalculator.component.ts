c49cc7835e871f80d941b8a9aff16d8052f86213
Bhautik Changani <bhautik.changani@etatvasoft.com>
2024-10-04 11:49:05 +0530

    budget calc

--- src/app/budget-calc/budgetcalculator/budgetcalculator.component.ts
+++ src/app/budget-calc/budgetcalculator/budgetcalculator.component.ts
@@ -7,10 +7,10 @@ interface BudgetRow {
   placeHolder?: string;
   categoryType?: string;
   values: (string | number | null)[];
-  isParentRow? : boolean;
+  isParentRow?: boolean;
   categories?: Category[];
-  isOpen? : boolean;
-  isInputRow? : boolean;
+  isOpen?: boolean;
+  isInputRow?: boolean;
 }
 
 interface Category {
@@ -43,23 +43,23 @@ export class BudgetcalculatorComponent {
   currentInputValue: number | null = null;
   incomeCategory: Category[] = [];
   expenseCategory: Category[] = [];
-
-  lastAddedInputRef!: ElementRef;
+  lastAddedInnerCategoryIndex: number | null = null;
 
   @ViewChild(MatMenuTrigger) contextMenuTrigger!: MatMenuTrigger;
+  @ViewChildren('categoryInput') inputRefs!: QueryList<ElementRef>;
 
-  constructor(private cdr : ChangeDetectorRef) {
+  constructor(private cdr: ChangeDetectorRef) {
     const today: Date = new Date();
     this.minEndDate = today.toISOString().split('T')[0];
   }
 
-  onStartDateChange() : void {
+  onStartDateChange(): void {
     if (this.startDate) {
       this.minEndDate = this.startDate;
     }
   }
 
-  submitDates() : void {
+  submitDates(): void {
     this.errorMessage = '';
 
     if (!this.startDate || !this.endDate) {
@@ -75,7 +75,7 @@ export class BudgetcalculatorComponent {
     this.updateTable();
   }
 
-  updateTotals() : void {
+  updateTotals(): void {
     const incomeTotals = Array(this.displayedColumns.length - 1).fill(0);
     const expenseTotals = Array(this.displayedColumns.length - 1).fill(0);
     const profitLoss = Array(this.displayedColumns.length - 1).fill(0);
@@ -84,13 +84,13 @@ export class BudgetcalculatorComponent {
 
     this.dataSource.find(x => x.category == 'Income')?.categories?.forEach(row => {
       row.values.forEach((value, index) => {
-        const numValue : number = parseFloat(value as string);
+        const numValue: number = parseFloat(value as string);
         incomeTotals[index] += numValue;
       })
     });
     this.dataSource.find(x => x.category == 'Expense')?.categories?.forEach(row => {
       row.values.forEach((value, index) => {
-        const numValue : number = parseFloat(value as string);
+        const numValue: number = parseFloat(value as string);
         expenseTotals[index] += numValue;
       })
     });
@@ -127,16 +127,16 @@ export class BudgetcalculatorComponent {
     });
   }
 
-  updateTable() : void {
-    const months : string[] = this.getMonthsBetween(new Date(this.startDate), new Date(this.endDate));
+  updateTable(): void {
+    const months: string[] = this.getMonthsBetween(new Date(this.startDate), new Date(this.endDate));
     this.displayedColumns = ['categories', ...months, 'action'];
     this.displayedColumns1 = ['categories', ...months];
     this.dataSource = [
-      { category: 'Income', type: 'title', values: Array(months.length).fill(''), isParentRow : true, categories : [], isOpen : true },
-      { category: 'Input', categoryType: 'income', placeHolder: 'Enter income category', type: 'title', values: Array(months.length).fill(''), isInputRow : true },
+      { category: 'Income', type: 'title', values: Array(months.length).fill(''), isParentRow: true, categories: [], isOpen: true },
+      { category: 'Input', categoryType: 'income', placeHolder: 'Enter income category', type: 'title', values: Array(months.length).fill(''), isInputRow: true },
       { category: 'Income Total', type: 'value', values: Array(months.length).fill(0) },
-      { category: 'Expense', type: 'title', values: Array(months.length).fill(''), isParentRow : true, categories : [], isOpen : true },
-      { category: 'Input', categoryType: 'expense', placeHolder: 'Enter expense category', type: 'title', values: Array(months.length).fill(''), isInputRow : true },
+      { category: 'Expense', type: 'title', values: Array(months.length).fill(''), isParentRow: true, categories: [], isOpen: true },
+      { category: 'Input', categoryType: 'expense', placeHolder: 'Enter expense category', type: 'title', values: Array(months.length).fill(''), isInputRow: true },
       { category: 'Expense Total', type: 'value', values: Array(months.length).fill(0) },
       { category: 'Profit / Loss', type: 'value', values: Array(months.length).fill(0) },
       { category: 'Opening Balance', type: 'value', values: Array(months.length).fill(0) },
@@ -145,13 +145,13 @@ export class BudgetcalculatorComponent {
     this.updateTotals();
   }
 
-  
-  openCategory(type : string){
-      this.dataSource.forEach(row => {
-        if(row.category == type){
-          row.isOpen  = (!row.isOpen)
-        }
-      })
+
+  openCategory(type: string) {
+    this.dataSource.forEach(row => {
+      if (row.category == type) {
+        row.isOpen = (!row.isOpen)
+      }
+    })
   }
 
   getMonthsBetween(startDate: Date, endDate: Date): string[] {
@@ -164,7 +164,7 @@ export class BudgetcalculatorComponent {
     return months;
   }
 
-  reset() : void {
+  reset(): void {
     this.isSubmit = false;
     this.startDate = '';
     this.endDate = '';
@@ -194,44 +194,50 @@ export class BudgetcalculatorComponent {
 
         this.incomeCategory.push({
           type: isIncome ? 'Income' : 'Expense',
-          name : inputValue,
-          values : Array(this.displayedColumns.length - 1).fill(0)
+          name: inputValue,
+          values: Array(this.displayedColumns.length - 1).fill(0)
         });
-        this.dataSource.find(x => x.category == (isIncome ? 'Income' : 'Expense'))?.categories?.push({
-          type: isIncome ? 'Income' : 'Expense',
-          name : inputValue,
-          values : Array(this.displayedColumns.length - 1).fill(0)
-        });
-
-        const inputRowIndex: number = this.dataSource.indexOf(row);
-        this.openCategory(isIncome ? 'Income' : 'Expense');
-        this.openCategory(isIncome ? 'Income' : 'Expense');
+        const categoryList = this.dataSource.find(x => x.category == (isIncome ? 'Income' : 'Expense'))?.categories || [];
+        this.dataSource.find(x => x.category == (isIncome ? 'Income' : 'Expense'))!.categories = [
+          ...categoryList,
+          {
+            type: isIncome ? 'Income' : 'Expense',
+            name: inputValue,
+            values: Array(this.displayedColumns.length - 1).fill(0)
+          }
+        ];
         (event.target as HTMLInputElement).value = '';
-        this.dataSource = [...this.dataSource];
-        
+        const categoryCount = this.dataSource.find(x => x.category == (isIncome ? 'Income' : 'Expense'))!.categories?.length || 0;
+      const focusIndex = (categoryCount - 1) * (this.displayedColumns.length - 2);
+
+      // Find the correct table for focusing
+      setTimeout(() => {
         setTimeout(() => {
-          setTimeout(() => {
-            const newRow : HTMLTableRowElement = document.querySelectorAll('tr')[inputRowIndex + 1];
-            if (newRow) {
-              const inputField : Element | null = newRow.querySelector('input[type="number"]');
-              if (inputField instanceof HTMLInputElement) {
-                inputField.focus();
-              }
+          const categoryRow = this.dataSource.find(x => x.category === (isIncome ? 'Income' : 'Expense'));
+          const rowIndex = this.dataSource.indexOf(categoryRow!);
+          const newRow: HTMLTableRowElement = document.querySelectorAll('tr')[rowIndex + categoryCount + 1];
+
+          if (newRow) {
+            const inputField: Element | null = newRow.querySelector('input[type="number"]');
+            if (inputField instanceof HTMLInputElement) {
+              inputField.focus();
             }
-          }, 0);
+          }
         }, 0);
+      }, 0);
+        
         this.updateTotals();
       }
     }
   }
 
-  onKeyDown(event: KeyboardEvent) : void {
+  onKeyDown(event: KeyboardEvent): void {
     if (event.key === 'Tab') {
       event.preventDefault();
     }
   }
 
-  openMenu(event: MouseEvent, menuTrigger: MatMenuTrigger, row: Category, value: number) : void {
+  openMenu(event: MouseEvent, menuTrigger: MatMenuTrigger, row: Category, value: number): void {
     event.preventDefault();
 
     this.contextMenuTrigger = menuTrigger;
@@ -240,42 +246,43 @@ export class BudgetcalculatorComponent {
 
     menuTrigger.openMenu();
 
-     const inputElement = event.target as HTMLElement;
+    const inputElement = event.target as HTMLElement;
     const rect = inputElement.getBoundingClientRect();
 
     setTimeout(() => {
-        const overlay = document.querySelector('.cdk-overlay-connected-position-bounding-box') as HTMLElement;
-        if (overlay) {
-            overlay.style.left = `${rect.left}px`;
-            overlay.style.top = `${rect.bottom}px`; 
-        }
+      const overlay = document.querySelector('.cdk-overlay-connected-position-bounding-box') as HTMLElement;
+      if (overlay) {
+        overlay.style.left = `${rect.left}px`;
+        overlay.style.top = `${rect.bottom}px`;
+      }
     }, 0);
-}
+  }
 
 
-  applyAll() : void {
+  applyAll(): void {
     if (this.selectedRow && this.currentInputValue !== null) {
       this.dataSource.find(x => x.category == this.selectedRow?.type)?.categories?.forEach(row => {
         if (row.name === this.selectedRow?.name) {
           row.values = row.values.map(() => this.currentInputValue);
         }
       });
-        this.cdr.detectChanges();
+      this.cdr.detectChanges();
       this.updateTotals();
     }
   }
 
-  deleteCategory(row: Category) : void {
-   const rowIndex = this.dataSource.find(x => x.category == row.type)?.categories?.indexOf(row);
-    if(row.type === 'Income'){
+  deleteCategory(row: Category): void {
+    const rowIndex = this.dataSource.find(x => x.category == row.type)?.categories?.indexOf(row);
+    if (row.type === 'Income') {
       this.existingIncomeCategories.delete(row.name.toLowerCase());
     } else if (row.type === 'Expense') {
       this.existingExpenseCategories.delete(row.name.toLowerCase());
     }
 
     if (rowIndex !== -1) {
-      this.dataSource.find(x => x.category == row.type)?.categories?.splice(Number(rowIndex), 1);
-      this.dataSource = [...this.dataSource];
+      this.dataSource.find(x => x.category == row.type)?.categories?.splice(Number(rowIndex), 1) || [];
+      const categories = this.dataSource.find(x => x.category == row.type)?.categories || [];
+      this.dataSource.find(x => x.category == row.type)!.categories = [...categories];
     }
     this.updateTotals();
     this.cdr.detectChanges();
